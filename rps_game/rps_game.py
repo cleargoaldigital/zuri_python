2be423c95fa53cb153ff9e49829d7be043f2d08d
cleargoaldigital <cleargoal.ng@gmail.com>
2022-05-31 10:59:44 +0100

    edited rps_game

--- rps_game/rps_game.py
+++ rps_game/rps_game.py
@@ -1,48 +1,81 @@
+#import random module
+import random
+
 # Rock-Paper_Scissors Game
 # A simple 2-player game.
-# Rock beat Scissors
-# Scissors beats Paper
-# paper beats Rock
+# Rock beats Scissors because it can crush scissors.
+# Scissors beats Paper because it can cut paper into pieces.
+# Paper beats Rock because it can wrap rock.
 
-# If players chose the same object at the same time, they tie.
+print("Welcome to Rock-Paper-Scissors Game")
+print("General rules of the game: \n"
+    +"Rock vs paper -> paper wins \n"
+    + "Rock vs scissor -> Rock wins \n"
+    +"paper vs scissor -> scissor wins \n")
 
-R = "rock"
-P = "Paper"
-S = "Scissors"
+while True:
+    # take the input from user
+    print("Select a weapon: \n R for Rock, \n P for Paper, and \n S for Scissors \n")
 
-# is_running = True
+    # accept input from user
+    playerChoice = input("Select your weapon: ").upper()
 
-# while is_running:
-options = ["R", "P", "S"]
+    if playerChoice == "R":
+        user_choice = "Rock"
+    elif playerChoice == "P":
+        user_choice = "Paper"
+    else:
+        user_choice = "Scissors"
 
-playerSelect = input("Enter any of R/P/S...").upper()
+    # print user's choice
+    print("Player choice is: ", playerChoice, ":", user_choice)
+    print("\nNow it's computer turn.......")
 
-ComputerSelect = random.choice(options)
+    #computer selects weapon randomly
+    options = ["R", "P", "S"]
+    computerChoice = random.choice(options)
 
+    if computerChoice == "R":
+        auto_choice = "Rock"
+    elif computerChoice == "P":
+        auto_choice = "Paper"
+    else:
+        auto_choice = "Scissors"
 
-if PlayerSelect == ComputerSelect:
-   print("The game is a tie!")
+    # print computer's choice
+    print("Computer choice is: ", computerChoice,":", auto_choice)
 
-elif
-   playerSelect == "P" && ComputerSelect == "R":
-   print("Congratulations. You have won the game!")
+    # display players' choices
+    print(user_choice + " VS " + auto_choice)
 
-elif
-   playerSelect == "R" && ComputerSelect == "P":
-   print("Oh Sorry! Computer has won this game!")
+    # compare weapons and display results
+    if playerChoice == computerChoice:
+        print("The game is a tie!", end = "")
 
-elif
-   playerSelect == "S" && ComputerSelect == "R":
-   print("Oh Sorry! Computer has won this game!")
+    elif playerChoice == "P" and computerChoice == "R":
+        print("Congratulations. You have won the game!", end = "")
 
-elif
-   playerSelect == "S" && ComputerSelect == "P":
-   print("Congratulations. You have won the game!")
+    elif playerChoice == "R" and computerChoice == "P":
+        print("Oh Sorry! Computer has won this game!", end = "")
 
-elif
-   playerSelect == "P" && ComputerSelect == "S":
-   print("Oh Sorry! Computer has won this game!")
+    elif playerChoice == "S" and computerChoice == "R":
+        print("Oh Sorry! Computer has won this game!", end = "")
 
-elif
-   playerSelect == "R" && ComputerSelect == "S":
-   print("Congratulations. You have won the game!")
+    elif playerChoice == "S" and computerChoice == "P":
+        print("Congratulations. You have won the game!", end = "")
+
+    elif playerChoice == "P" and computerChoice == "S":
+        print("Oh Sorry! Computer has won this game!", end = "")
+
+    elif playerChoice == "R" and computerChoice == "S":
+        print("Congratulations. You have won the game!", end = "")
+
+    # ask if user wants to exit or continue the game
+    print("\nDo you want to play again? (Y/N)")
+    user_response = input().lower()
+  
+    # user chooses n to exit the game
+    if user_response == 'n':
+        break
+    # goodbye message after the game.
+print("\nThanks you for taking time to play this incredible game.")
